f39181e3dd2ed82b1fc226ad10e56b8b5404edd3
AungKhant <aung.khant@ucsy.edu.mm>
2021-12-31 17:18:34 +0630

    order&place doughnutChart

--- js/dashboard.js
+++ js/dashboard.js
@@ -42,13 +42,13 @@ const myChart = new Chart(ctx, {
     options: {
         scales: {
             yAxes: [{
-                // display: false,
+                display: false,
                 ticks: {
                     beginAtZero: true
                 }
             }],
             xAxes: [{
-                // display: false,
+                display: false,
                 gridLines: [{
                     display: false
                 }]
@@ -56,8 +56,63 @@ const myChart = new Chart(ctx, {
         },
         legend: {
             display: true,
-            shape: "circle",
             position: 'top',
+            labels: {
+                fontColor: '#333',
+                usePointStyle: true
+            }
+        }
+    }
+});
+
+let orderFromPlace = [5,15,4,9,7];
+let places = ["YGN","MDY","NPY","SHAN","MGW"];
+
+const op = document.getElementById('op').getContext('2d');
+const opChart = new Chart(op, {
+    type: 'doughnut',
+    data: {
+        labels: places,
+        datasets: [{
+            label: 'Order & Place',
+            data: orderFromPlace,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                display: false,
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [{
+                display: false,
+                gridLines: [{
+                    display: false
+                }]
+            }]
+        },
+        legend: {
+            display: true,
+            position: 'bottom',
             labels: {
                 fontColor: '#333',
                 usePointStyle: true
